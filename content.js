ef3d7c7e6514a23dab10a544f67de74d044cee30
Adrien Joly <adrien@whyd.com>
2017-05-28 21:02:50 +0200

    fetches and displays checklists from my main trello board

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+const TRELLO_APP_KEY = '0b15414357140fe88faecea94f0a22b1';
+const TRELLO_BOARD_ID = '57a481755ab2c09495ba5c3b'; // TODO: allow user to select
 const img = chrome.extension.getURL('icon.png');
 
 const isReminder = (node) =>
@@ -6,22 +8,71 @@ const isReminder = (node) =>
 
 var showOptions = () => console.warn('showOptions is not ready yet...');
 
-InboxSDK.load('2', 'sdk_inbox-to-trello_97faaf3ffb').then(function(sdk){
-  /*
-  sdk.Conversations.registerMessageViewHandlerAll(function(view) {
-    console.log('MessageViewHandlerAll', view);
+// Trello API helpers
+
+function authorizeTrello(authenticationSuccess, authenticationFailure) {
+  Trello.authorize({
+    type: 'popup',
+    name: 'Inbox Reminders to Trello',
+    scope: {
+      read: 'true',
+      write: 'true' },
+    expiration: 'never',
+    success: authenticationSuccess,
+    error: authenticationFailure
   });
-  sdk.Lists.registerThreadRowViewHandler(attachLinks);
-  */
+}
+
+function fetchChecklists(boardId, callback) {
+  const url = `boards/${boardId}/checklists`; //'members/me/boards'
+  const options = {
+    cards: 'all',
+    card_fields: 'name,shortUrl',
+  };
+  Trello.rest('GET', url, options, callback, callback);
+}
+
+InboxSDK.loadScript('https://api.trello.com/1/client.js?key=' + TRELLO_APP_KEY);
+
+// Main UI: move a reminder to Trello
+
+InboxSDK.load('2', 'sdk_inbox-to-trello_97faaf3ffb').then(function(sdk){
   showOptions = function(reminderName) {
-    const el = document.createElement('div');
-    el.innerHTML = reminderName;
+    const el = document.createElement('ul');
+    el.appendChild(document.createTextNode('title: ' + reminderName));
+    const btn = document.createElement('button');
+    btn.appendChild(document.createTextNode('load checklists from trello'));
+
+    function fetchAndAppendChecklists() {
+      authorizeTrello(function() {
+        console.log('trello is connected!');
+        fetchChecklists(TRELLO_BOARD_ID, function(res) {
+          if (!res.length) {
+            console.error(res);
+          } else {
+            el.removeChild(el.getElementsByTagName('button')[0]);
+            res.forEach((checklist) => {
+              const li = document.createElement('li');
+              li.appendChild(document.createTextNode(checklist.cards[0].name + ' - ' + checklist.name));
+              el.appendChild(li);
+            })
+          }
+        });
+      }, console.error)
+    }
+
+    fetchAndAppendChecklists();
+
+    btn.onclick = fetchAndAppendChecklists;
+    el.appendChild(btn);
     sdk.Widgets.showModalView({
       el,
       title: 'Move Reminder to Trello Card',
     });
   };
 });
+
+// Add Trello button to all reminders from Google Inbox UI
 
 //document.querySelector('.scroll-list-section-body')
 document.body.addEventListener('DOMSubtreeModified', function (ev) {
